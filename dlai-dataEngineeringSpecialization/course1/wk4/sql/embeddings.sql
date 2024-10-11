d8849bd3e7fbcb96115481dab37bd5eed4d07108
M Lincon <49597604+mlincon@users.noreply.github.com>
2024-10-11 21:38:51 +0000

    updates

--- dlai-dataEngineeringSpecialization/course1/wk4/sql/embeddings.sql
+++ dlai-dataEngineeringSpecialization/course1/wk4/sql/embeddings.sql
@@ -1,24 +1,34 @@
 CREATE EXTENSION IF NOT EXISTS aws_s3 CASCADE;
 CREATE EXTENSION IF NOT EXISTS vector;
 
+
 DROP TABLE IF EXISTS item_emb;
+CREATE TABLE IF NOT EXISTS item_emb (
+    id VARCHAR PRIMARY KEY,
+    embedding VECTOR(32)
+);
+
+SELECT aws_s3.table_import_from_s3(
+    'item_emb',
+    'id,embedding',
+    '(format csv, header true)',
+    'de-c1w4-905418182499-us-east-1-ml-artifacts',
+    'embeddings/item_embeddings.csv',
+    'us-east-1'
+);
+
+
 DROP TABLE IF EXISTS user_emb;
-
-CREATE TABLE IF NOT EXISTS item_emb (id varchar PRIMARY KEY, embedding vector(32));
-CREATE TABLE IF NOT EXISTS user_emb (id int PRIMARY KEY, embedding vector(32));
-
-SELECT aws_s3.table_import_from_s3(
-   'item_emb', 'id,embedding', '(format csv, header true)',
-   '<BUCKET_NAME>',
-   'embeddings/item_embeddings.csv', 
-   'us-east-1'
+CREATE TABLE IF NOT EXISTS user_emb (
+    id INT PRIMARY KEY,
+    embedding VECTOR(32)
 );
 
-
 SELECT aws_s3.table_import_from_s3(
-   'user_emb', 'id,embedding', '(format csv, header true)',
-   '<BUCKET_NAME>',
-   'embeddings/user_embeddings.csv', 
-   'us-east-1'
+    'user_emb',
+    'id,embedding',
+    '(format csv, header true)',
+    'de-c1w4-905418182499-us-east-1-ml-artifacts',
+    'embeddings/user_embeddings.csv',
+    'us-east-1'
 );
-
